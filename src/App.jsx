6ebc537d80194990e74278019db14fbe3ee6781b
AminouZakara <aminouzakara@gmail.com>
2023-10-12 14:57:47 +0300

    changes done in App.jsx on tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className=' flex justify-center'>
-      <h1 className='mt-32  text-4xl font-bold text-green-600'>This Is My Tailwind Demo Page</h1>
+    <div className='bg-green-900 p-5 flex justify-center'>
+      <div className="container-wrapper p-5 bg-green-400">
+        <div className="container bg-green-200">
+
+          <div className="header">
+            <h2>Welcome to our website!</h2>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   )
 }
