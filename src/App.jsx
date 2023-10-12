80dd2974ce817a68bc93bfd37a759544695251d0
AminouZakara <aminouzakara@gmail.com>
2023-10-12 14:41:19 +0300

    changes on Web Name

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' flex justify-center'>
+      <h1 className='mt-32  text-4xl font-bold text-green-600'>This Is My Tailwind Demo Page</h1>
+    </div>
+  )
+}
+
+export default App
