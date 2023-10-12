fa36b5a33b0976f9269cdf620cad3adf935e659a
AminouZakara <aminouzakara@gmail.com>
2023-10-12 15:28:25 +0300

    Changes done in App.jsx on layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-green-900 p-5 flex justify-center'>
-      <div className="container-wrapper p-5 bg-green-400">
-        <div className="container bg-green-200">
+    <div className='bg-green-900 h-screen w-screen p-4'>
+      <div className="container-wrapper h-screen flex justify-center  w-screen bg-green-400 p-2">
+        <div className="container p-3 grid grid-cols-3 gap-3 ">
 
-          <div className="header">
+          <div className="header bg-green-200 p-3 rounded col-span-full">
             <h2>Welcome to our website!</h2>
+          </div>
+
+          <div className="banner bg-green-200 p-3 rounded col-span-full">
+            <h3>Banner</h3>
+          </div>
+
+          <div className="content bg-green-200 p-3 rounded col-span-2">
+            <h3>Content</h3>
+          </div>
+
+          <div className="adds bg-green-200 p-3 rounded">
+            <h3>Adds</h3>
+          </div>
+
+          <div className="footer bg-green-200 p-3 rounded col-span-full">
+            <h3>Footer</h3>
           </div>
 
         </div>
